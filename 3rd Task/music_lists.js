1ada1139baa812369738c6807ae18d4039519bfe
Harsh Maru <108467595+harshmaru25@users.noreply.github.com>
2023-07-23 21:05:23 +0530

    Add files via upload

---
+++ 3rd Task/music_lists.js
@@ -0,0 +1,39 @@
+let allMusic = [
+    {
+        name: "Cradles",
+        artist: "Sub Urban",
+        img: "music2",
+        src: "music1"
+    },
+    {
+        name: "Interworld - Metamorphosis",
+        artist: "Interworld",
+        img: "music1",
+        src: "music2"
+    },
+    {
+        name: " DANGEROUS",
+        artist: "ANGELPLAYA",
+        img: "music3",
+        src: "music3"
+    },
+    {
+        name: "on & on - cartoon",
+        artist: "Daniel Levi",
+        img: "music4",
+        src: "music4"
+    },
+    {
+        name: " Mortals",
+        artist: "Warriyo",
+        img: "music5",
+        src: "music5"
+    },
+    {
+        name: "Last Summer",
+        artist: "Ikson",
+        img: "music6",
+        src: "music6"
+    }
+
+]
